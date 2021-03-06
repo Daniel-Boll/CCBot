54cecdd4cb38e0063124e009bec58a3f8cebbc0e
Daniel Boll <danielboll.academico@gmail.com>
2021-03-06 10:07:09 -0300

    Preventing reactions to fail

--- src/reactions.js
+++ src/reactions.js
@@ -6,5 +6,6 @@ const reactions = { regras, materias };
 module.exports = (reaction, user, action) => {
   const channel = reaction.message.channel.name;
 
-  reactions[channel](reaction, user, action);
+  if (reactions.hasOwnProperty(channel))
+    reactions[channel](reaction, user, action);
 };
