46720668194e127ada6e499897309414c1669f2b
Daniel Boll <danielboll.academico@gmail.com>
2021-03-09 10:08:25 -0300

    Changing server to don't need GET request to operate

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,7 @@
+require("core-js/stable");
+require("regenerator-runtime/runtime");
+require("dotenv").config();
+
 const Discord = require("discord.js");
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
@@ -6,51 +10,39 @@ const client = new Discord.Client({
 const commandHandler = require("./commands");
 const reactionHandler = require("./reactions");
 
-require("dotenv").config();
+client.login(process.env.BOT_TOKEN);
 
-module.exports.awake = () => {
-  client.login(process.env.BOT_TOKEN);
-};
+client.on("message", (message) => {
+  if (message.content.startsWith("!")) {
+    commandHandler(message);
+  }
+});
 
-module.exports.listen = () => {
-  client.on("message", (message) => {
-    if (message.content.startsWith("!")) {
-      commandHandler(message);
+client.on("messageReactionAdd", async (reaction, user) => {
+  // When we receive a reaction we check if the reaction is partial or not
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message: ", error);
+      // Return as `reaction.message.author` may be undefined/null
+      return;
     }
-  });
+  }
+  reactionHandler(reaction, user, "add");
+});
 
-  client.on("messageReactionAdd", async (reaction, user) => {
-    // When we receive a reaction we check if the reaction is partial or not
-    if (reaction.partial) {
-      // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-      try {
-        await reaction.fetch();
-      } catch (error) {
-        console.error(
-          "Something went wrong when fetching the message: ",
-          error
-        );
-        // Return as `reaction.message.author` may be undefined/null
-        return;
-      }
+client.on("messageReactionRemove", async (reaction, user) => {
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message: ", error);
+      return;
     }
-    reactionHandler(reaction, user, "add");
-  });
+  }
+  reactionHandler(reaction, user, "remove");
+});
 
-  client.on("messageReactionRemove", async (reaction, user) => {
-    if (reaction.partial) {
-      try {
-        await reaction.fetch();
-      } catch (error) {
-        console.error(
-          "Something went wrong when fetching the message: ",
-          error
-        );
-        return;
-      }
-    }
-    reactionHandler(reaction, user, "remove");
-  });
-
-  return { status: "Listening" };
-};
+return { status: "Listening" };
